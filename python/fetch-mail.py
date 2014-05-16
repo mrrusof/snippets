2b45c5a8172ef33ca5dd800d3d852e4d40ebd25d
Ruslan Ledesma-Garza <ruslanledesmagarza@gmail.com>
2014-05-15 19:14:56 -0500

    ERROR -> FAILURE

--- python/fetch-mail.py
+++ python/fetch-mail.py
@@ -4,33 +4,33 @@ import sys
 import imaplib
 import email
 
-# 1. Fetch last email from imap.gmail.com
+# 1. Fetch most recent email from imap.gmail.com
 M = imaplib.IMAP4_SSL('imap.gmail.com', 993)
 addr = "ruslanledesmagarza@gmail.com"
 pwd = raw_input("Password for "+addr+": ")
 try:
     M.login(addr, pwd)
 except imaplib.IMAP4.error:
-    print "ERROR: login to", addr, "failed"
+    print "FAILURE: could not login to", addr
     sys.exit(1)
 rv, _ = M.select("Inbox")
 if rv != "OK":
-    print "ERROR: could not select Inbox"
+    print "FAILURE: could not select Inbox"
     sys.exit(2)
 rv, msgnums = M.search(None, "ALL")
 if rv != "OK":
-    print "ERROR: no messages found in Inbox (1)"
+    print "FAILURE: no messages found in Inbox (1)"
     sys.exit(3)
 if len(msgnums) == 0:
-    print "ERROR: no messages found in Inbox (2)"
+    print "FAILURE: no messages found in Inbox (2)"
     sys.exit(4)
 msgnums = msgnums[0].split()
 if len(msgnums) == 0:
-    print "ERROR: no messages found in Inbox (3)"
+    print "FAILURE: no messages found in Inbox (3)"
     sys.exit(5)
 rv, rawmsg = M.fetch(msgnums[-1], "(RFC822)")
 if rv != "OK":
-    print "ERROR: failed to get message", msgnums[-1]
+    print "FAILURE: could not get message", msgnums[-1]
     sys.exit(6)
 msg = email.message_from_string(rawmsg[0][1])
 print
