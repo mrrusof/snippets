32a042179b2c5c687b17081d790c141e0cc1a8d1
Ruslan Ledesma-Garza <ruslanledesmagarza@gmail.com>
2014-05-20 00:05:38 -0500

    reverse w/o swap

--- c/reverse/reverse.c
+++ c/reverse/reverse.c
@@ -2,12 +2,22 @@
 
 void reverse(char *s) {
   int l = 0;
+  for(; *(s+l) != 0; l++);
   char swap;
-  for (; *(s+l) != 0; l++);
-  for(int i = 0; l > i; i++) {
+  for(int i = 0; i < l; i++) {
     swap = *(s+(--l));
     *(s+l) = *(s+i);
     *(s+i) = swap;
+  }
+}
+
+void reverse_wo_swap(char *s) {
+  int l = 0;
+  for(; *(s+l) != 0; l++);
+  for(int i = 0; i < l; i++) {
+    *(s+i) = *(s+i) + *(s+(--l));
+    *(s+l) = *(s+i) - *(s+l);
+    *(s+i) = *(s+i) - *(s+l);
   }
 }
 
@@ -16,5 +26,7 @@ int main() {
   printf("Original string: \"%s\"\n", mystring);
   reverse(mystring);
   printf("Reversed string: \"%s\"\n", mystring);
+  reverse_wo_swap(mystring);
+  printf("Previous string reversed w/o swap: \"%s\"\n", mystring);
   return 0;
 }
