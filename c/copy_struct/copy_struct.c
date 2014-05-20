a0d9b778774e1ecc071c544b6b3005fa67d59812
Ruslan Ledesma-Garza <ruslanledesmagarza@gmail.com>
2014-05-20 01:23:31 -0500

    copy int field

--- c/copy_struct/copy_struct.c
+++ c/copy_struct/copy_struct.c
@@ -12,6 +12,7 @@ int main() {
   printf("m.a      = %d\nm.s[0]   = %d\nm.s[1]   = %d\n\n", m.a, m.s[0], m.s[1]);
   mp = (MSG *) malloc(sizeof(MSG));
   printf("mp->a    = %d\nmp->s[0] = %d\nmp->s[1] = %d\n\n", mp->a, mp->s[0], mp->s[1]);
+  mp->a = m.a;
   char *fp, *tp;
   for(fp = (char *)m.s, tp = (char *)mp->s; tp < (char *)(mp+1);)
         *tp++ = *fp++;
