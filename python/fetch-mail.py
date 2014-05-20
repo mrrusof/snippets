b9e690eac95a65ff96471b4c968739621171e427
Ruslan Ledesma-Garza <ruslanledesmagarza@gmail.com>
2014-05-19 20:27:57 -0500

    better messages in fetch-mail.py

--- python/fetch-mail.py
+++ python/fetch-mail.py
@@ -3,47 +3,44 @@
 import sys
 import imaplib
 import email
+import log
 
 # 1. Fetch most recent email from imap.gmail.com
 M = imaplib.IMAP4_SSL('imap.gmail.com', 993)
 addr = "ruslanledesmagarza@gmail.com"
-pwd = raw_input("Password for "+addr+": ")
+pwd = raw_input('Password for %s: ' % addr)
+log.event('looking for new messages')
 try:
     M.login(addr, pwd)
 except imaplib.IMAP4.error:
-    print "FAILURE: could not login to", addr
-    sys.exit(1)
-rv, _ = M.select("Inbox")
-if rv != "OK":
-    print "FAILURE: could not select Inbox"
-    sys.exit(2)
-rv, msgnums = M.search(None, "ALL")
-if rv != "OK":
-    print "FAILURE: no messages found in Inbox (1)"
-    sys.exit(3)
+    log.failwith('could not login to %s' % addr)
+rv, _ = M.select('Inbox')
+if rv != 'OK':
+    log.failwith('could not select Inbox')
+rv, msgnums = M.search(None, 'ALL')
+if rv != 'OK':
+    log.failwith('search for new messages failed')
 if len(msgnums) == 0:
-    print "FAILURE: no messages found in Inbox (2)"
-    sys.exit(4)
+    log.failwith('search for new messages returned no messages')
 msgnums = msgnums[0].split()
 if len(msgnums) == 0:
-    print "FAILURE: no messages found in Inbox (3)"
-    sys.exit(5)
-rv, rawmsg = M.fetch(msgnums[-1], "(RFC822)")
-if rv != "OK":
-    print "FAILURE: could not get message", msgnums[-1]
-    sys.exit(6)
+    log.event('there are no new messages')
+    sys.exit(0)
+rv, rawmsg = M.fetch(msgnums[-1], '(RFC822)')
+if rv != 'OK':
+    log.failwith('could not get message %s' % msgnums[-1])
 msg = email.message_from_string(rawmsg[0][1])
 print
-print "Msg Number:", msgnums[-1]
-print "Subject:   ", msg["Subject"]
-print "Date:      ", msg["Date"]
+print 'Msg Number:', msgnums[-1]
+print 'Subject:   ', msg['Subject']
+print 'Date:      ', msg['Date']
 if not msg.is_multipart():
-    print "Content-type:",msg.get_content_type()
+    print 'Content-type:',msg.get_content_type()
     print
     print msg.get_payload()
 else:
     for part in msg.walk():
-        print "--------------------------------------------------"
-        print "Conten-type:",part.get_content_type()
+        print '--------------------------------------------------'
+        print 'Conten-type:',part.get_content_type()
         print
         print part.get_payload()
