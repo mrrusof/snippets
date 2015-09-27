aeb6cfc34db5bbda0e58760dc6b787ed3f29192d
Ruslan Ledesma-Garza <ruslanledesmagarza@gmail.com>
2015-09-27 14:55:35 -0500

    Basic chat in Ruby.

---
+++ ruby/cookbook/tcp-server.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+require 'gserver'
+
+class ChatServer < GServer
+
+  def initialize(port=2060, host=GServer::DEFAULT_HOST)
+    @clients = []
+    super(port, host, Float::MAX, $stderr, true)
+  end
+
+  def serve(sock)
+    begin
+      @clients << sock
+      hostname = sock.peeraddr[2] || sock.peeraddr[3]
+      @clients.each do |c|
+        c.puts "#{hostname} has joined the chat." unless c == sock
+      end
+      until sock.eof? do
+        message = sock.gets.chomp
+        break if message == "/quit"
+        @clients.each { |c| c.puts "#{hostname}: #{message}" unless c == sock }
+      end
+    ensure
+      @clients.delete(sock)
+      @clients.each { |c| c.puts "#{hostname} has left the chat." }
+    end
+  end
+end
+
+server = ChatServer.new(*ARGV[0..2] || 20606)
+server.start(-1)
+server.join
